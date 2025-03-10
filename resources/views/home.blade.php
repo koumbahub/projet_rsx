250539dea2dec3cd286c2a811e60d1e8d56ea552
Koumba DIALLO <koumbadiallo564@gmail.com>
2025-03-10 19:43:02 +0000

    second commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,39 +21,4 @@
         </div>
     </div>
 
-    <div class="row mt-5">
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-primary text-white mb-4">
-                <div class="card-body">Nombre d'employés</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <span>{{ $nombreEmployees }}</span>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-success text-white mb-4">
-                <div class="card-body">Nombre de clients</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <span>{{ $nombreClients }}</span>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-warning text-white mb-4">
-                <div class="card-body">Nombre de documents partagés</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <span>{{ $nombreDocuments }}</span>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-3 col-md-6">
-            <div class="card bg-danger text-white mb-4">
-                <div class="card-body">Alertes/Erreurs</div>
-                <div class="card-footer d-flex align-items-center justify-content-between">
-                    <span>{{ $nombreAlertes }}</span>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+    
